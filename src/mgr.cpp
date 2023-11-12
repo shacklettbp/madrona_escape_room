3a8686628f2c3ebdf046afe43025b73428bc5865
Zander <majercik@cs.stanford.edu>
2023-11-12 14:07:24 -0800

    fixed train interface

--- src/mgr.cpp
+++ src/mgr.cpp
@@ -638,6 +638,8 @@ TrainInterface Manager::trainInterface() const
         rewardTensor(),
         doneTensor(),
         resetTensor(),
+        checkpointTensor(),
+        checkpointResetTensor(),
         Optional<Tensor>::none(),
     };
 }
