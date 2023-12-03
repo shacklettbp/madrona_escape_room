144055b1b8260c18d18572ff3764deb5b5f2d854
Brennan Shacklett <bpshacklett@gmail.com>
2023-12-02 21:46:00 -0800

    Add pytorch rgb / depth tensor bindings

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -46,6 +46,8 @@ NB_MODULE(madrona_escape_room, m) {
              &Manager::doorObservationTensor)
         .def("lidar_tensor", &Manager::lidarTensor)
         .def("steps_remaining_tensor", &Manager::stepsRemainingTensor)
+        .def("rgb_tensor", &Manager::rgbTensor)
+        .def("depth_tensor", &Manager::depthTensor)
     ;
 }
 
