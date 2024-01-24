5bf2f23080390bfc331164817f31de808b8ef80c
Brennan Shacklett <bpshacklett@gmail.com>
2024-01-23 20:14:00 -0800

    Add rand_seed arg

--- scripts/jax_train.py
+++ scripts/jax_train.py
@@ -55,6 +55,7 @@ sim = madrona_escape_room.SimManager(
     gpu_id = args.gpu_id,
     num_worlds = args.num_worlds,
     auto_reset = True,
+    rand_seed = 5,
 )
 
 jax_gpu = jax.devices()[0].platform == 'gpu'
